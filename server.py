a99b464e195006abc486d01256532c1da43267f7
Fortieratm <fortierformation@gmail.com>
2026-03-22 19:31:01 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import http.server, socketserver, os
+
 PORT = int(os.environ.get("PORT", 8080))
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 handler = http.server.SimpleHTTPRequestHandler
-with socketserver.TCPServer(("", PORT), handler) as httpd:
-    print(f"Serving on port {PORT}")
-    httpd.serve_forever()
+httpd = socketserver.TCPServer(("0.0.0.0", PORT), handler)
+print(f"Serving on 0.0.0.0:{PORT}")
+httpd.serve_forever()
